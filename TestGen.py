3829e4fc49ac74eb30e867d261a592577528f271
colour-me-bibi <bennyforeman1@gmail.com>
2021-07-12 12:18:15 -0700

    Fixed docx generation

--- TestGen.py
+++ TestGen.py
@@ -6,10 +6,6 @@
 __author__ = "Benjamin Foreman (bennyforeman1@gmail.com)"
 
 
-# TODO test generation
-# TODO tests to pdfs
-
-
 import itertools as it
 import json
 import random
@@ -17,7 +13,7 @@ from dataclasses import dataclass, field
 from enum import Enum, auto
 from string import ascii_uppercase
 
-# from dataclasses_serialization.json import JSONSerializer
+from docx import Document
 
 
 class Difficulty(Enum):
@@ -91,7 +87,6 @@ class Question:
         kwargs = {
             "prompt": question["prompt"],
             "question_type": QuestionType.from_str(question["question_type"]),
-            "is_required": question["is_required"],
         }
 
         if "boolean" in question:
@@ -99,6 +94,9 @@ class Question:
 
         if "answers" in question:
             kwargs["answers"] = [Answer.deserialize_answer(answer) for answer in question["answers"]]
+
+        if "correct_response" in question:
+            kwargs["correct_response"] = question["correct_response"]
 
         if "difficulty" in question:
             kwargs["difficulty"] = Difficulty.from_str(question["difficulty"])
@@ -151,11 +149,11 @@ class Test:
                 self.answer_key.append(str(question.boolean))
             elif question.question_type == QuestionType.MULTIPLE_CHOICE:
                 self.answer_key.append(
-                    " or ".join(ascii_uppercase[i] for i, answer in question.answers if answer.is_correct)
+                    " or ".join(ascii_uppercase[i] for i, answer in enumerate(question.answers) if answer.is_correct)
                 )
-            elif question.question_type == QuestionType.MULTIPLE_CHOICE:
+            elif question.question_type == QuestionType.SELECT_ALL:
                 self.answer_key.append(
-                    " and ".join(ascii_uppercase[i] for i, answer in question.answers if answer.is_correct)
+                    " and ".join(ascii_uppercase[i] for i, answer in enumerate(question.answers) if answer.is_correct)
                 )
             elif question.question_type == QuestionType.SHORT_ANSWER:
                 self.answer_key.append(question.correct_response)
@@ -164,19 +162,25 @@ class Test:
 
 
 def generate_n_tests(testbank: TestBank, n: int, ratio: tuple[int, int, int]):
-    required, nonrequired = it.groupby(testbank.questions, key=lambda question: question.is_required)
-    easy, medium, hard = it.groupby(nonrequired, key=lambda question: question.difficulty)
+    required = [question for question in testbank.questions if question.is_required]
+    nonrequired = [question for question in testbank.questions if not question.is_required]
+
+    easy = [question for question in nonrequired if question.difficulty == Difficulty.EASY]
+    medium = [question for question in nonrequired if question.difficulty == Difficulty.MEDIUM]
+    hard = [question for question in nonrequired if question.difficulty == Difficulty.HARD]
+
+    # required, nonrequired = tuple(it.groupby(testbank.questions, key=lambda question: question.is_required))
+    # easy, medium, hard = tuple(it.groupby(nonrequired, key=lambda question: question.difficulty))
 
     multiple = 1
     while (
-        len(easy) < (multiple + 1) * ratio[0]
-        and len(medium) < (multiple + 1) * ratio[1]
-        and len(hard) < (multiple + 1) * ratio[2]
+        len(easy) >= (multiple + 1) * ratio[0]
+        and len(medium) >= (multiple + 1) * ratio[1]
+        and len(hard) >= (multiple + 1) * ratio[2]
     ):
         multiple += 1
 
     tests = list()
-
     for i in range(1, n + 1):
         questions = (
             required
@@ -195,11 +199,43 @@ def generate_n_tests(testbank: TestBank, n: int, ratio: tuple[int, int, int]):
     return tests
 
 
-def tests_to_doc(tests: list[Test], filename: str = None):
-    pass
+def tests_to_doc(tests: list[Test], title: str):
+    document = Document()
+
+    for test_idx, test in enumerate(tests, 1):
+        document.add_heading(f"{title} (ID: {test_idx})", 0)
+
+        for question_idx, question in enumerate(test.questions, 1):
+            document.add_heading(f"{question_idx} - {question.prompt}", level=1)
+
+            if question.question_type == QuestionType.BOOLEAN:
+                document.add_paragraph("\tTrue or False")
+            elif question.question_type in (QuestionType.MULTIPLE_CHOICE, QuestionType.SELECT_ALL):
+                for answer_idx, answer in enumerate(question.answers):
+                    document.add_paragraph(f"\t{ascii_uppercase[answer_idx]} - {answer.text}")
+            elif question.question_type == QuestionType.SHORT_ANSWER:
+                for _ in range(2):
+                    document.add_paragraph()
+            else:
+                raise NotImplementedError
+
+        document.add_page_break()
+
+    for test_idx, test in enumerate(tests, 1):
+        document.add_heading(f"{title} (ID: {test_idx})", 0)
+
+        for answer_idx, answer in enumerate(test.answer_key):
+            document.add_paragraph(f"{answer_idx} - {answer}")
+
+        document.add_paragraph()
+        document.add_page_break()
+
+    document.save(f"{title}-{len(tests)}.docx")
 
 
 if __name__ == "__main__":
-    test = testbank_from_file("example_test.json")
+    testbank = testbank_from_file("example_test.json")
 
-    print(test.title)
+    tests = generate_n_tests(testbank, 5, (1, 1, 1))
+
+    tests_to_doc(tests, testbank.title)
